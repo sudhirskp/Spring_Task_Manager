144b63a69f8cf745dd63722ae669e69544a3628f
sudhirskp <gnitwallah@gmail.com>
2025-07-07 15:45:12 +0530

    add taskEntities class for task management

---
+++ Spring.Manage/src/main/java/com/spring/Spring/Manage/Entities/taskEntities.java
@@ -0,0 +1,14 @@
+package com.spring.Spring.Manage.Entities;
+
+
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class taskEntities {
+    private int id;
+    private String Title;
+    private String description;
+    private Date DeadLine;
+}
