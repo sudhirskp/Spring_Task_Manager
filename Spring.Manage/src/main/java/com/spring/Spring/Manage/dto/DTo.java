a2bc8562a246842beeaf3dae638b4d0a9762e429
sudhirskp <gnitwallah@gmail.com>
2025-07-07 16:40:14 +0530

    add DTo class for data transfer object representation

---
+++ Spring.Manage/src/main/java/com/spring/Spring/Manage/dto/DTo.java
@@ -0,0 +1,15 @@
+package com.spring.Spring.Manage.dto;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class DTo {
+    private String title;
+    private String description;
+    private String deadline;
+}
