0f0728a22e12064dac357f3a5512ca53d636859e
sudhirskp <gnitwallah@gmail.com>
2025-07-08 21:29:04 +0530

    add updateTask endpoint and enhance error handling in TaskController

--- Spring.Manage/src/main/java/com/spring/Spring/Manage/Controller/TaskController.java
+++ Spring.Manage/src/main/java/com/spring/Spring/Manage/Controller/TaskController.java
@@ -2,10 +2,13 @@ package com.spring.Spring.Manage.Controller;
 
 import com.spring.Spring.Manage.Entities.taskEntities;
 import com.spring.Spring.Manage.Service.TaskService;
-import com.spring.Spring.Manage.dto.DTo;
+import com.spring.Spring.Manage.dto.CreateTaskdto;
+import com.spring.Spring.Manage.dto.ErrorResponseDto;
+import com.spring.Spring.Manage.dto.UpdateDto;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.text.ParseException;
 import java.util.List;
 
 @RestController
@@ -34,9 +37,28 @@ public class TaskController {
         return ResponseEntity.ok(taskbyid);
     }
 
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<taskEntities> updateTask(@PathVariable("id") Integer id, @RequestBody UpdateDto body) throws ParseException {
+        var task = taskService.updateTask(id , body.getDescription() , body.getDeadline() , body.getCompleted());
+        if(task == null){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(task);
+    }
+
    @PostMapping("/addTask")
-   public ResponseEntity<taskEntities> addTask(@RequestBody DTo body) {
+   public ResponseEntity<taskEntities> addTask(@RequestBody CreateTaskdto body) throws ParseException {
        var task = taskService.addTask(body.getTitle(), body.getDescription(), body.getDeadline());
        return ResponseEntity.ok(task);
+   }
+
+   @ExceptionHandler(Exception.class)
+   public ResponseEntity<ErrorResponseDto> handleException(Exception ex) {
+
+        if(ex instanceof ParseException) return ResponseEntity.badRequest().body(new ErrorResponseDto("invalid date format"));
+
+        ex.printStackTrace(); // for dev side
+       return ResponseEntity.internalServerError().body(new ErrorResponseDto("something went wrong"));
    }
 }
